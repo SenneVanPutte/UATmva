265c7fe8c34085a13a9978274252f4a8bf0cb5ac
xjanssen <>
2011-06-23 11:53:01 +0000

    CS var list

--- UATmva.C
+++ UATmva.C
@@ -28,7 +28,7 @@ void UATmva(TString Cfg = "Config.cfg" , TString Steps = "TRS" ){
   // MVA Reading (Evaluation)
   if ( Steps.Contains ('R') ) {
     UATmvaReader Reader;
-//    Reader.SetNbin(44); 
+    Reader.SetNbin(44); 
     Reader.SetNbin(1);
     Reader.Do(Config,Tree); 
   }  
